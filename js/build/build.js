d052fda8771fb0cfc5e6899e98dfd76a49b084ab
Kevin Roark <ker2143@columbia.edu>
2015-08-24 22:33:57 -0400

    start work on arranged and draggable tools

--- js/build/build.js
+++ js/build/build.js
@@ -17,13 +17,59 @@
   }
 
   function startRaking() {
-    $('#title-screen').fadeOut(2000);
+    $('#title-screen').fadeOut(1000);
 
     var $rake = $('#rake');
-    $(document).mousemove(function(ev) {
-      $rake.css('left', (ev.pageX - $rake.width() / 2) + 'px');
-      $rake.css('top', (ev.pageY - $rake.height() / 2) + 'px');
+
+    var $poems = $('.poem');
+    arrangeCards($poems, $('#poems'));
+
+    var $activePoem;
+    $poems.mousedown(function() {
+      $activePoem = $(this);
     });
+    $poems.mouseup(function() {
+      $activePoem = null;
+    });
+
+    var mouseState = {};
+    $(document).mousemove(function(ev) {
+      followCursor($rake, ev);
+
+      if ($activePoem) {
+        var offset = $activePoem.offset();
+
+        var dx = ev.pageX - mouseState.x;
+        var left = parseInt(offset.left + dx);
+        console.log(left);
+        $activePoem.css('left', left + 'px');
+
+        var dy = ev.pageY - mouseState.y;
+        var top = offset.top - dy;
+        $activePoem.css('top', top + 'px');
+      }
+
+      mouseState.x = ev.pageX; mouseState.y = ev.pageY;
+    });
+  }
+
+  function followCursor($el, ev) {
+    $el.css('left', (ev.pageX - $el.width() / 2) + 'px');
+    $el.css('top', (ev.pageY - $el.height() / 2) + 'px');
+  }
+
+  function arrangeCards($cards, $container) {
+    for (var i = 0; i < $cards.length; i++) {
+      var $card = $($cards[i]);
+
+      $card.css('position', 'absolute');
+
+      var left = parseInt(Math.random() * ($container.width() - $card.width()));
+      $card.css('left', left);
+
+      var top = parseInt(Math.random() * ($container.height() - $card.height()));
+      $card.css('top', top);
+    }
   }
 
 })();
